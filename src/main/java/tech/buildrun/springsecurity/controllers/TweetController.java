9048e0c72502224731768db7c68acfc130d30e8f
Ernany Gonçalves <goncalves.ernany@gmail.com>
2024-06-03 16:59:25 -0300

    Delete /tweets/{id}

--- src/main/java/tech/buildrun/springsecurity/controllers/TweetController.java
+++ src/main/java/tech/buildrun/springsecurity/controllers/TweetController.java
@@ -1,11 +1,12 @@
 package tech.buildrun.springsecurity.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import tech.buildrun.springsecurity.controllers.dtos.CreateTweetDTO;
+import tech.buildrun.springsecurity.entities.Role;
 import tech.buildrun.springsecurity.entities.Tweet;
 import tech.buildrun.springsecurity.repositories.TweetRepository;
 import tech.buildrun.springsecurity.repositories.UserRepository;
@@ -30,6 +31,24 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,JwtAuthenticationToken token){
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var tweet = tweetRepository.findById(tweetId).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles().stream().anyMatch(role->role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if(isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))){
+            tweetRepository.deleteById(tweetId);
+        }else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+
 
         return ResponseEntity.ok().build();
     }
